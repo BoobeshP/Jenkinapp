be3dee018b9021c1f68a23176b78c3351c10c111
BoobeshP <boobeshp79@gmail.com>
2026-03-23 10:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def health():
 @app.route("/add/<int:a>/<int:b>")
 def add(a, b):
     return jsonify(result=a + b)
+    
+@app.route("/")
+def home():
+    return "Flask app is running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
